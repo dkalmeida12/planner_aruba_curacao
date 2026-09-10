0eb3046bee6f0f9dcd4d0ee54a80e6d3095d27e0
dkalmeida12 <dkalmeida12@gmail.com>
2026-09-10 19:28:49 -0300

    Add offline cache service worker

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='planner-aruba-curacao-v3';
+const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET')return;
+  const url=new URL(req.url);
+  if(url.origin!==self.location.origin)return;
+  event.respondWith(fetch(req).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(cache=>cache.put(req,copy));return resp}).catch(()=>caches.match(req).then(resp=>resp||caches.match('./index.html'))));
+});
